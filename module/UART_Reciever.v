ccd264726b02039368e5f18a23061899c1759293
neungkl <kosatelim@gmail.com>
2016-05-24 00:34:55 +0700

    Fix UART Reciever

--- module/UART_Reciever.v
+++ module/UART_Reciever.v
@@ -11,19 +11,17 @@ module UART_Reciever(
   wire checkBit;
 	reg [10:0] data_raw;
 	
-	assign checkBit = ^data;
+	reg [7:0] dd;
+	assign checkBit = ^dd;
   
 	reg [7:0] tmpData [0:3];
-	reg [1:0] idTmp;
-	
-	reg [2:0] count;
+	reg [1:0] count;
 	
 	initial begin
 		error = 0;
 		sent = 0;
-		count = 0;
-		idTmp = 0;
 		data_raw = 11'h7FF;
+		count = 0;
 	end
 	
   always @(posedge clk) begin
@@ -33,29 +31,27 @@ module UART_Reciever(
 		
 		if(!sent) begin
 			if(count != 0) begin
-				data = tmpData[idTmp];
+				data = tmpData[count-1];
 				sent = 1;
 			end
 		end
 		else begin
 			if(recieved) begin
-				sent = 0;
 				count = count - 1;
-				idTmp = idTmp + 1;
+				sent = 0;
 			end
 		end
 		
 		if(data_raw[10] == 0) begin
 			
 			error = 0;
-			data = {data_raw[2], data_raw[3], data_raw[4], data_raw[5], data_raw[6], data_raw[7], data_raw[8], data_raw[9]};
+			dd = {data_raw[2], data_raw[3], data_raw[4], data_raw[5], data_raw[6], data_raw[7], data_raw[8], data_raw[9]};
 			
 			if(data_raw[1] != checkBit) error = 1;
 			else if(data_raw[0] != 1) error = 2;
 			
-			if(error == 0 && count != 4) begin
-				tmpData[idTmp] = data;
-				idTmp = idTmp + 1;
+			if(error == 0 && count != 3) begin
+				tmpData[count] = dd;
 				count = count + 1;
 			end
 			
